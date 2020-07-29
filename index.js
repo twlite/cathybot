739d70809dc042a910b8ded513c94fd5d5ed314e
Snowflake <devsnowflake107@gmail.com>
2020-07-29 23:56:43 +0545

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const client = new Client({
     disableMentions: "all"
 });
 const cathy = new Bot();
+const messages = [
+    "Sorry, I can't understand.",
+    "What do you mean?",
+    "uh",
+    "I love you :D",
+    "Why D:",
+    "Wut?",
+    "Hmm.",
+    "idot",
+    "idot, wdym?"
+];
 
 client.on("ready", () => {
     console.log("Bot is ready!");
@@ -18,16 +29,16 @@ cathy.on("ready", () => {
 client.on("message", async message => {
     if (message.author.bot || !message.guild) return;
     if (message.channel.id === "738092276051607703") {
-        message.channel.startTyping();
+        message.channel.startTyping(3);
         cathy.chat(message.content)
         .then(msg => {
-            if (!msg.content) return message.channel.send("idot, wdym?");
+            if (!msg.content) return message.channel.send(messages[Math.floor(Math.random() * messages.length)]);
             message.channel.stopTyping(true);
             return message.channel.send(msg.content);
         })
         .catch(e => {
             message.channel.stopTyping(true);
-            return message.channel.send("idot, wdym?");
+            return message.channel.send(messages[Math.floor(Math.random() * messages.length)]);
         });
     }
 });
